e2009ec1c6ecb3c30e47f7802dfc76ee63f1e3e2
ddominguezbl <ddominguezbl@uoc.edu>
2021-05-26 19:23:08 +0200

    Entrega final Producto 3

---
+++ app/src/main/java/com/daviddo/pruebadelogin/InterfaceDAOFirebase.java
@@ -0,0 +1,10 @@
+package com.daviddo.pruebadelogin;
+
+import java.util.List;
+
+public interface InterfaceDAOFirebase {
+
+    public void afterReadAllElements(List lista);
+    public void afterReadAllImagenes(List lista);
+
+}
